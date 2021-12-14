d93650c90b8b455590aa24eb6584af403f613bf5
yuancf1024 <3455911441@qq.com>
2021-12-14 14:53:10 +0800

    v5重构-提取测试代码的公共部分到一个新函数中checkSums

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"testing"
 	"reflect"
+	"testing"
 )
 
 func TestSum(t *testing.T) {
@@ -44,22 +44,24 @@ func TestSum(t *testing.T) {
 
 func TestSumAllTails(t *testing.T) {
 
-	t.Run("make the sums of some slices", func(t *testing.T) {
-		got := SumAllTails([]int{1, 2}, []int{0, 9})
-		want := []int{2, 9}
-
+	// 我们的测试代码有一部分是重复的，我们可以把它放到另一个函数中复用。
+	checkSums := func(t *testing.T, got, want []int) {
 		if !reflect.DeepEqual(got, want) {
 			t.Errorf("got %v want %v", got, want)
 		}
+	}
+
+	t.Run("make the sums of some slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+		checkSums(t, got, want)
+		// checkSums(t, got, "dave")
 	})
 
 	t.Run("safely sum empty slices", func(t *testing.T) {
 		got := SumAllTails([]int{}, []int{3, 4, 5})
 		want := []int{0, 9}
 		// want := "bob"
-
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("got %v want %v", got, want)
-		}
+		checkSums(t, got, want)
 	})
 }

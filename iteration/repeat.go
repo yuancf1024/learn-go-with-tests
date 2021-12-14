d0d144e0a4830515a87ab3d9634226f34a4d6ec0
yuancf1024 <3455911441@qq.com>
2021-12-14 10:32:10 +0800

    迭代v2-调用者可以指定字符重复的次数

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,9 +1,9 @@
 package iteration
 
-const repeatCount = 5
+// const repeatCount = 5
 
 // Repeat returns character repeated 5 times.
-func Repeat(character string) string {
+func Repeat(character string, repeatCount int) string {
 	var repeated string
 	for i := 0; i < repeatCount; i++ {
 		repeated += character

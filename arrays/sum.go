eeff81280040db54b79e90752504f5d6ee3fa648
yuancf1024 <3455911441@qq.com>
2021-12-14 14:12:08 +0800

    v4-重构 使用 append 函数，它能为切片追加一个新值。

--- arrays/sum.go
+++ arrays/sum.go
@@ -8,13 +8,17 @@ func Sum(numbers []int) int {
 	return sum
 }
 
-func sumAll(numbersToSum ...[]int) (sums []int) {
-	lengthOfNumbers := len(numbersToSum)
-	sums = make([]int, lengthOfNumbers)
+func sumAll(numbersToSum ...[]int) []int {
+	// lengthOfNumbers := len(numbersToSum)
+	// sums = make([]int, lengthOfNumbers)
 
-	for i, numbers := range numbersToSum {
-		sums[i] = Sum(numbers)
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		// sums[i] = Sum(numbers)
+		// 使用 append 函数，它能为切片追加一个新值。
+		sums = append(sums, Sum(numbers))
 	}
 
-	return
+	return sums
 }

0116ace4e7f6f11d8459ef8820f88bc18e9c4f8d
yuancf1024 <3455911441@qq.com>
2022-01-04 16:27:13 +0800

    v3-Add不会修改现有值

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -4,7 +4,11 @@ import "errors"
 
 type Dictionary map[string]string
 
-var ErrNotFound = errors.New("counld not find the word you were looking for")
+var (
+	ErrNotFound = errors.New("counld not find the word you were looking for")
+	ErrWordExists = errors.New("cannot add word because it already exists")
+)
+
 
 // 定义字典的搜索函数
 func (d Dictionary) Search(word string) (string, error) {
@@ -16,6 +20,18 @@ func (d Dictionary) Search(word string) (string, error) {
 }
 
 // 定义字典的添加函数功能
-func (d Dictionary) Add(word, definition string) {
-	d[word] = definition
+func (d Dictionary) Add(word, definition string) error {
+
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+	
+	return nil
 }

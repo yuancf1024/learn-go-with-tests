ddaa4e65375a8db8f79c798dd4cb0f75acf893de
yuancf1024 <3455911441@qq.com>
2022-01-05 17:08:52 +0800

    v2add 它打印 2，1 然后输出「Go!」

--- mocking/main.go
+++ mocking/main.go
@@ -7,7 +7,13 @@ import (
 )
 
 func Countdown(out io.Writer) {
-	fmt.Fprint(out, "3")
+	for i := 3; i > 0; i-- {
+		fmt.Fprintln(out, i)
+		// Fprintln格式使用其操作数的默认格式并写入io.Writer。
+		// 在操作数之间总是添加空格，并添加换行符。
+		// 它返回写入的字节数和遇到的任何写入错误。
+	}
+	fmt.Fprint(out, "Go!")
 }
 
 func main() {

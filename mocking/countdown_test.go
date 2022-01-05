13bdb1a3ebeeb175b2753dbbbb788056743a3a6c
yuancf1024 <3455911441@qq.com>
2022-01-05 19:52:17 +0800

    v3 使用依赖注入的方式代替休眠

--- mocking/countdown_test.go
+++ mocking/countdown_test.go
@@ -9,8 +9,9 @@ func TestCountdown(t *testing.T) {
 
 	// bytes 包中的 buffer 类型实现了 Writer 接口。
 	buffer := &bytes.Buffer{} // Buffer是一个实现了读写方法的可变大小的字节缓冲。
+	spySleeper := &SpySleeper{}
 
-	Countdown(buffer)
+	Countdown(buffer, spySleeper)
 
 	got := buffer.String()
 	want := `3
@@ -20,6 +21,10 @@ Go!`
 
 	if got != want {
 		t.Errorf("got '%s' want '%s'", got, want)
+	}
+
+	if spySleeper.Calls != 4 {
+		t.Errorf("not enough calls to sleeper, want 4 got %d", spySleeper.Calls)
 	}
 
 }
--- mocking/main.go
+++ mocking/main.go
@@ -10,19 +10,40 @@ import (
 const finalWord = "Go!"
 const countdownStart = 3
 
-func Countdown(out io.Writer) {
+type Sleeper interface {
+	Sleep()
+}
+
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
+type ConfigurableSleeper struct {
+	duration time.Duration
+}
+
+func (o *ConfigurableSleeper) Sleep() {
+	time.Sleep(o.duration)
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := countdownStart; i > 0; i-- {
-		time.Sleep(1 * time.Second)
+		sleeper.Sleep()
 		fmt.Fprintln(out, i)
 		// Fprintln格式使用其操作数的默认格式并写入io.Writer。
 		// 在操作数之间总是添加空格，并添加换行符。
 		// 它返回写入的字节数和遇到的任何写入错误。
 	}
 
-	time.Sleep(1 * time.Second)
+	sleeper.Sleep()
 	fmt.Fprint(out, finalWord)
 }
 
 func main() {
-	Countdown(os.Stdout)
+	sleeper := &ConfigurableSleeper{1 * time.Second}
+	Countdown(os.Stdout, sleeper)
 }

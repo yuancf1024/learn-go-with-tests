bc39c33ce5f611380813ce7931a8397e1d5ce955
yuancf1024 <3455911441@qq.com>
2022-01-05 19:09:32 +0800

    v2 重构-将变量重构为命名常量+add 1秒的暂停

--- mocking/main.go
+++ mocking/main.go
@@ -4,16 +4,23 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
 
+const finalWord = "Go!"
+const countdownStart = 3
+
 func Countdown(out io.Writer) {
-	for i := 3; i > 0; i-- {
+	for i := countdownStart; i > 0; i-- {
+		time.Sleep(1 * time.Second)
 		fmt.Fprintln(out, i)
 		// Fprintln格式使用其操作数的默认格式并写入io.Writer。
 		// 在操作数之间总是添加空格，并添加换行符。
 		// 它返回写入的字节数和遇到的任何写入错误。
 	}
-	fmt.Fprint(out, "Go!")
+
+	time.Sleep(1 * time.Second)
+	fmt.Fprint(out, finalWord)
 }
 
 func main() {

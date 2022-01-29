af46acd9b8c33255026f19921d151054a3a1fa4c
yuancf1024 <3455911441@qq.com>
2022-01-29 21:28:31 +0800

    v2模拟测试

--- select/racer_test.go
+++ select/racer_test.go
@@ -2,11 +2,25 @@ package racer
 
 import (
 	"testing"
+	"net/http"
+	"net/http/httptest"
+	"time"
 )
 
 func TestRacer(t *testing.T) {
-	slowURL := "http://www.facebook.com"
-	fastURL := "http://www.quii.co.uk"
+
+	slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(20 * time.Millisecond)
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	fastServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	slowURL := slowServer.URL
+	
+	fastURL := fastServer.URL
 
 	want := fastURL
 	got := Racer(slowURL, fastURL)
@@ -14,4 +28,7 @@ func TestRacer(t *testing.T) {
 	if got != want {
 		t.Errorf("got '%s', want '%s'", got, want)
 	}
+
+	slowServer.Close()
+	fastServer.Close()
 }

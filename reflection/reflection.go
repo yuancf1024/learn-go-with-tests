042ad04bf5a91a310a4ebb7a16cd4727c92a37d4
yuancf1024 <3455911441@qq.com>
2022-02-12 21:09:37 +0800

    v2为测试用例添加2个字符字段场景

--- reflection/reflection.go
+++ reflection/reflection.go
@@ -5,7 +5,12 @@ import "reflect"
 func walk(x interface{}, fn func(input string)) {
 	val := reflect.ValueOf(x) 
 	// ValueOf返回一个新值，初始化为存储在接口i中的具体值。ValueOf(nil)返回零值。 
-	field := val.Field(0)
+	
+	// val.NumField 返回值中的字段数
+	for i := 0; i < val.NumField(); i++ {
+		field := val.Field(i)
+		fn(field.String())
+	}
 	// Field返回结构v的第i个字段。如果v的类型不是struct或i不在范围内，它会panics。
-	fn(field.String())
+	
 }
--- reflection/reflection_test.go
+++ reflection/reflection_test.go
@@ -1,28 +1,45 @@
 package main
 
 import (
-	// "reflect"
+	"reflect"
 	"testing"
 )
 
 func TestWalk(t *testing.T) {
-	
-	expected := "Chris"
-	var got []string
 
-	x := struct {
+	cases := []struct{
 		Name string
-	}{expected}
-
-	walk(x, func(input string) {
-		got = append(got, input)
-	})
-
-	if len(got) != 1 {
-		t.Errorf("wrong number of function calls, got %d want %d", len(got), 1)
+		Input interface{}
+		ExpectedCalls []string
+	} {
+		{
+			"Struct with one string field",
+			struct {
+				Name string
+			}{"Chris"},
+			[]string{"Chris"},
+		},
+		{
+			"Struct with two string fields",
+			struct {
+				Name string
+				City string
+			}{"Chris", "London"},
+			[]string{"Chris", "London"},
+		},
 	}
+	
+	for _, test := range cases {
+		t.Run(test.Name, func(t *testing.T) {
+			var got []string
+			walk(test.Input, func(input string) {
+				got = append(got, input)
+			})
 
-	if got[0] != expected {
-		t.Errorf("got '%s', want '%s'", got[0], expected)
+			// got和测试案例期望的调用结果不一致，则报错分别输出其
+			if !reflect.DeepEqual(got, test.ExpectedCalls) {
+				t.Errorf("got %v, want %v", got, test.ExpectedCalls)
+			}
+		})
 	}
 }

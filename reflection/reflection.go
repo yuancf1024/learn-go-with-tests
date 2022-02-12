bdd2715b5b4563c8a6f28c7440e896321f26d2c4
yuancf1024 <3455911441@qq.com>
2022-02-12 21:49:02 +0800

    重构-封装一个获得 reflect.Value 的功能

--- reflection/reflection.go
+++ reflection/reflection.go
@@ -3,15 +3,8 @@ package main
 import "reflect"
 
 func walk(x interface{}, fn func(input string)) {
-	val := reflect.ValueOf(x) 
-	// ValueOf返回一个新值，初始化为存储在接口i中的具体值。ValueOf(nil)返回零值。 
-	
-	// val.NumField 返回值中的字段数
+	val := getValue(x)
 
-	if val.Kind() == reflect.Ptr {
-		val = val.Elem()
-	}
-	
 	for i := 0; i < val.NumField(); i++ {
 		field := val.Field(i)
 
@@ -23,4 +16,14 @@ func walk(x interface{}, fn func(input string)) {
 		}	
 	}
 	// Field返回结构v的第i个字段。如果v的类型不是struct或i不在范围内，它会panics。
+}
+
+func getValue(x interface{}) reflect.Value {
+	val := reflect.ValueOf(x)
+
+	if val.Kind() == reflect.Ptr {
+		val = val.Elem()
+	}
+
+	return val
 }

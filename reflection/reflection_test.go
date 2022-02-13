9a5c2c3ad6720d948bb3f2ba50808a59201d2b4a
yuancf1024 <3455911441@qq.com>
2022-02-13 10:54:44 +0800

    v6测试用例覆盖数组类型

--- reflection/reflection_test.go
+++ reflection/reflection_test.go
@@ -69,6 +69,14 @@ func TestWalk(t *testing.T) {
 			},
 			[]string{"London", "Reykjavik"},
 		},
+		{
+			"Arrays",
+			[2]Profile {
+				{33, "London"},
+				{34, "Reykjavik"},
+			},
+			[]string{"London", "Reykjavik"},
+		},
 	}
 	
 	for _, test := range cases {

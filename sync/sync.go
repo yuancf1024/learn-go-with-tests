1ff096ce11b62828666903f94ee7d1d4c068aa68
yuancf1024 <3455911441@qq.com>
2022-02-17 16:18:58 +0800

    sync

--- sync/sync.go
+++ sync/sync.go
@@ -1,13 +1,26 @@
 package v1
 
+import "sync"
+
+// Counter将会自增1
 type Counter struct {
+	mu    sync.Mutex
 	value int
 }
 
+// NewCounter 返回一个新的计数器
+func NewCounter() *Counter {
+	return &Counter{}
+}
+
+// 计数器自增
 func (c *Counter) Inc() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
 	c.value++
 }
 
+// 返回当前计数
 func (c *Counter) Value() int {
 	return c.value
 }

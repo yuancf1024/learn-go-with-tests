9ac2f025a65b6a1febc490ba7a9a5e58221608a1
yuancf1024 <3455911441@qq.com>
2022-02-18 19:22:48 +0800

    v1

--- context/context.go
+++ context/context.go
@@ -7,10 +7,24 @@ import (
 
 type Store interface {
 	Fetch() string
+	Cancel()
 }
 
 func Server(store Store) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, store.Fetch())
+		ctx := r.Context()
+
+		data := make(chan string, 1)
+
+		go func() {
+			data <- store.Fetch()
+		}()
+
+		select {
+		case d := <-data:
+			fmt.Fprint(w, d)
+		case <- ctx.Done():
+			store.Cancel()
+		}
 	}
 }
--- context/context_test.go
+++ context/context_test.go
@@ -1,23 +1,80 @@
 package context
 
-type StubStore struct {
+import (
+	"testing"
+	"time"
+	"net/http"
+	"net/http/httptest"
+	"context"
+	// "errors"
+)
+
+type SpyStore struct {
 	response string
+	cancelled bool
+	t *testing.T
 }
 
-func (s *StubStore) Fetch() string {
+func (s *SpyStore) Fetch() string {
+	time.Sleep(100 * time.Millisecond)
 	return s.response
 }
 
-func TestServer(t *testing.T) {
-	data := "hello, world"
-	svr := Server(&StubStore{data})
+func (s *SpyStore) Cancel() {
+	s.cancelled = true
+}
 
-	request := httptest.NewRequest(http.MethodGet, "/", nil)
-	response := httptest.NewRecorder() // 返回一个初始化了的ResponseRecorder
-
-	svr.ServeHTTP(response, request)
-
-	if response.Body.String() != data {
-		t.Errorf('got "%s", want "%s"', response.Body.String(), data)
+func (s *SpyStore) assertWasCancelled() {
+	s.t.Helper()
+	if !s.cancelled {
+		s.t.Error("store was not told to cancel")
 	}
+}
+
+func (s *SpyStore) assertWasNotCancelled() {
+	s.t.Helper()
+	if s.cancelled {
+		s.t.Error("store was told to cancel")
+	}
+}
+
+func TestServer(t *testing.T) {
+
+	data := "hello, world"
+
+	t.Run("returns data from store", func(t *testing.T) {
+		
+		store := &SpyStore{response: data, t : t}
+		svr := Server(store)
+		
+		request := httptest.NewRequest(http.MethodGet, "/", nil)
+		response := httptest.NewRecorder() // 返回一个初始化了的ResponseRecorder
+		
+		svr.ServeHTTP(response, request)
+		
+		if response.Body.String() != data {
+			t.Errorf(`got "%s", want "%s"`, response.Body.String(), data)
+		}
+
+		store.assertWasNotCancelled()
+	})
+
+	
+	t.Run("tells store to cancel work if request is cancelled", func(t *testing.T) {
+		data := "hello, world"
+		store := &SpyStore{response: data, t : t}
+		svr := Server(store)
+		
+		request := httptest.NewRequest(http.MethodGet, "/", nil)
+		
+		cancellingCtx, cancel := context.WithCancel(request.Context())
+		time.AfterFunc(5 * time.Millisecond, cancel)
+		request = request.WithContext(cancellingCtx)
+		
+		response := httptest.NewRecorder() // 返回一个初始化了的ResponseRecorder
+		
+		svr.ServeHTTP(response, request)
+		
+		store.assertWasCancelled()
+	})
 }
